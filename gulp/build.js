3cdebd51f10a513d853be9a55df9181dbb0fb1dd
Vadym Maksyuk <maksyukvadim@gmail.com>
2016-07-22 17:54:49 +0300

    deleted task for minification css and html

--- gulp/build.js
+++ gulp/build.js
@@ -3,6 +3,6 @@ const gulpLoadPlugins = require('gulp-load-plugins');
 const $ = gulpLoadPlugins();
 const config = require('./config').path;
 
-gulp.task('build', [ 'html', 'images', 'fonts', 'copyDataLang'], () => {
+gulp.task('build', ['images', 'fonts', 'copyDataLang'], () => {
   return gulp.src(config.build.all).pipe($.size({title: 'build', gzip: true}));
 });
